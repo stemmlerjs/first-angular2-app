2a81323fc73b960427a507ea9ec9a6426a1807da
stemmlerjs <kstemmler@bcsglobal.ca>
2016-07-25 16:39:52 -0400

    This shit is just like react. awesome

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,11 @@
-import {Component} from 'angular2/core';
+import { Component } from 'angular2/core';
+import { CoursesComponent } from './courses.component'
 
+// We include other components by adding them to the directives attribute
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>Yo, this is a typescript app</h1><courses></courses>',
+    directives: [CoursesComponent] 
 })
+
 export class AppComponent { }
---
+++ app/courses.component.ts
@@ -0,0 +1,18 @@
+import { Component } from 'angular2/core'
+
+@Component({
+    selector: 'courses',
+    template: `
+        <h2>Courses</h2>
+            {{title}}    
+        <ul>
+            <li *ngFor="#course of courses">
+                {{course}}
+            </li>
+        </ul>
+    `
+})
+export class CoursesComponent {
+   title: string = "The title of courses page";
+   courses = ['Course1', 'Course2', 'Course3'];
+}
